8a2aa09f42d735f9b0e4110f6dd98e94fbb4a547
erko-perko <erci.gruden@gmail.com>
2025-10-29 11:32:57 +0100

    datoteke med vaje02

---
+++ 02-programski-jezik-go/koda/primeri/go.mod
@@ -0,0 +1,3 @@
+module primeri
+
+go 1.25.3
---
+++ 02-programski-jezik-go/koda/switch.go
@@ -0,0 +1,46 @@
+/*
+Program switch prikazuje kako uporabljamo stavek switch v jeziku go
+*/
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	// Tipičen primer stavka switch
+	// Ni potrebe po pisanju break ukaza
+	i := 2
+	switch i {
+	case 1:
+		fmt.Println("ena")
+	case 2:
+		fmt.Println("dva")
+	case 3:
+		fmt.Println("tri")
+	default:
+		fmt.Println("nič od naštetega")
+	}
+	// Izpuščen izraz: ekvivalentno stavku if-else
+	i = 3
+	switch {
+	case i == 1:
+		fmt.Println("ena")
+	case i == 2:
+		fmt.Println("dva")
+	case i == 3:
+		fmt.Println("tri")
+	default:
+		fmt.Println("nič od naštetega")
+	}
+	// Primer inicializacije
+	// Izraze lahko povežemo
+	switch dayOfTheWeek := time.Now().Weekday(); dayOfTheWeek {
+	case time.Saturday, time.Sunday:
+		fmt.Println("Vikend je!")
+	default:
+		fmt.Println("Na FRI bo treba!")
+	}
+}
